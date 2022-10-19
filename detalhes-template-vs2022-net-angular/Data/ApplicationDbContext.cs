d413c71b9d082c87de2c25daf0707a280503d808
julian-gambia-ensino <julian.gamboa.ms.2021@gmail.com>
2022-10-19 00:34:41 -0300

    Detalhes do Start https://learn.microsoft.com/en-us/visualstudio/javascript/tutorial-asp-net-core-with-angular?view=vs-2022

---
+++ detalhes-template-vs2022-net-angular/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using detalhes_template_vs2022_net_angular.Models;
+using Duende.IdentityServer.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace detalhes_template_vs2022_net_angular.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+
+        }
+    }
+}
---
+++ detalhes-template-vs2022-net-angular/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace detalhes_template_vs2022_net_angular.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
