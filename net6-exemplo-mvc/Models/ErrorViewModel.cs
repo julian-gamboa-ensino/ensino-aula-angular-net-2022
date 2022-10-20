55b91aa69ef24d24f4a0e96575101aa41ead58eb
julian-gambia-ensino <julian.gamboa.ms.2021@gmail.com>
2022-10-20 03:50:40 -0300

    Aprimorando o gitignore https://gist.github.com/indyfromoz/4109296

---
+++ net6-exemplo-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace net6_exemplo_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
